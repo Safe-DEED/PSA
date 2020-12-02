dfd4b503bdb11faf3ab863fa729a4315b9304029
Nick Angelou <nick.angelou@runpanther.io>
2020-12-02 10:14:45 -0600

    fix: tests pass

--- test/test.js
+++ test/test.js
@@ -27,7 +27,7 @@ describe('an psa lib', () => {
             [90], [91], [92], [93], [94], [95], [96], [97], [98], [99]];
 
     const encryptedArray = PSA.encrypt(array, clientContext)
-    const serializedGaloisKeys = PSA.getSerializedGaloisKeys(clientContext)
+    const serializedGaloisKeys = clientContext.galoisKeys.save()
     const out = PSA.compute(
       encryptedArray,
       serializedGaloisKeys,
